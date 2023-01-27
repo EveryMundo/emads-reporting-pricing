6f8c4dfae437442c4ccde5e9500b291ec6eef62c
Anahi Martinez <anahi@everymundo.com>
2023-01-27 06:59:40 -0500

    Separate reports

--- handler.js
+++ handler.js
@@ -9,12 +9,14 @@ const fHandler = require('./lib/fileHandler')
 
 const handler = async (event, context) => {
   try {
-    let tenants = await mongoHelper.getAllSearchEngineDbs()
-    // let tenants = ["a3"]
+    // let tenants = await mongoHelper.getAllSearchEngineDbs()
+    const reportForClient = `Client-Report_${new Date().toLocaleDateString().replace(/\//g, "_")}.xlsx`
+    const reportForDev = `Dev-Report_${new Date().toLocaleDateString().replace(/\//g, "_")}.xlsx`
+    let tenants = ["a3"]
     for (const tenant of tenants) {
-      let stringReport = ""
-      stringReport += `Routes report without prices ${new Date().toLocaleDateString()}\n`
-      stringReport += "tenant, Total routes without prices\n"
+
+      let stringReport = `Routes report without prices ${new Date().toLocaleDateString()}`
+      stringReport += "\ntenant, Total routes without prices"
       try {
         const { journeyType, lookaheadWindow } = await req.getSettings(tenant)
         console.log({ journeyType, lookaheadWindow })
@@ -45,39 +47,45 @@ const handler = async (event, context) => {
           }
 
         ])
-        stringReport += `${tenant},${routes.length}\n`
+        stringReport += `\n${tenant},${routes.length}\n`
         console.log(`${tenant},${routes.length}`)
-        await fHandler.appendToFile(`./reports/${tenant}-all-pricing-report_${new Date().toLocaleDateString().replace(/\//g, "_")}.xlsx`, stringReport)
-        stringReport = `\nOrigin, Destination, Currency, Available currencies, Report Notes\n`
+        await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
+        stringReport = `\nOrigin, Destination, Currency, Available currencies, Report Notes`
+        await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
         let i = 0;
         console.log(`${routes.length} routes`)
         for (const route of routes) {
           console.log(`Origin:${route._id.origin} with ${route.destinations.length} destinations`)
           const arrayOutput = inChunks(route.destinations, 100);
           for (const destinations of arrayOutput) {
-            // console.log({ destinations })
             const prices = await req.getPrices(tenant, journeyType, lookaheadWindow, route._id.origin, destinations.map(dest => dest.destination))
             for (const destInfo of destinations) {
               if (!prices.length) {
-                stringReport += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},0, No prices found with this currency\n`
+                stringReport = `\n${route._id.origin},${destInfo.destination},${destInfo.currency},0, No prices found with this currency`
+                await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
               } else {
                 // Review destination
                 const availableCurrencies = prices.filter(price => price.outboundFlight.arrivalAirportIataCode == destInfo.destination).map(price => price.priceSpecification.currencyCode)
                 const curcAvailable = req.isCurrencyAvailable(destInfo.currency, availableCurrencies)
-                const notes = curcAvailable ? "There was a price. This route needs review by development team"
-                  : "Low probability of this route having prices - update to a available currency if available"
-                stringReport += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}\n`
-                await fHandler.appendToFile(`./reports/${tenant}-all-pricing-report_${new Date().toLocaleDateString().replace(/\//g, "_")}.xlsx`, stringReport)
+
+                const notes = curcAvailable ? "Review by DevTeam"
+                  : "Low probability of this route having prices - update to a available currency if existent"
+                stringReport = `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}`
+                if (curcAvailable) {
+                  await fHandler.appendToFile(`./reports/${tenant}-${reportForDev}`, stringReport)
+                } else {
+                  await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
+                }
                 /**
                  * Get a detailed report
                  *
                  */
                 // if (!curcAvailable) {
                 //   const adgroups = await searchEngineOps.getAdGroupsV1(tenant, { "routeIdentifier.o": route._id.origin, "routeIdentifier.d": destInfo.destination, "routeIdentifier.curC": destInfo.currency })
-                //   stringReport = "\nAccountId, CampaignId, Campaign Name, AdGroup Id, AdGroup Name" + adgroups.map((adgroup) => {
-                //     return `${adgroup._id.accountId},${adgroup._id.campaignId},${adgroup.campaignName},${adgroup._id.adgroupId},${adgroup.name}`
+                //   stringReport = "\nAccountId, CampaignId, Campaign Name, AdGroup Id, AdGroup Name,Currency used, Sugested currencies\n" + adgroups.map((adgroup) => {
+                //     return `${adgroup._id.accountId},${adgroup._id.campaignId},${adgroup.campaignName},${adgroup._id.adgroupId},${adgroup.name},${adgroup.routeIdentifier.curC},${availableCurrencies.join(" ")}`
                 //   }).join("\n")
-                //   await fHandler.appendToFile(`./reports/${tenant}-all-pricing-report_${new Date().toLocaleDateString().replace(/\//g, "_")}.xlsx`, stringReport)
+                //   await fHandler.appendToFile(`./reports/${tenant}-detailed${reportForClient}`, stringReport)
                 // }
               }
             }
