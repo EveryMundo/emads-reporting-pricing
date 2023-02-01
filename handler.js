70ee2ce6fdf861def13510c878ca2227cbca4b46
Anahi Martinez <anahi@everymundo.com>
2023-01-31 20:36:39 -0500

    Update script

--- handler.js
+++ handler.js
@@ -50,100 +50,151 @@ const getDocsAggregation = (tenant) => {
       $group: {
         _id: {
           "origin": "$origin",
-          // "destination": "$destination",
+          "destination": "$destination",
+          "currency": "$priceFormat.isoCode",
         },
-        destinations: {
+        data: {
           $addToSet: {
+            "origin": "$origin",
             "destination": "$destination",
-            "currency": "$priceFormat.isoCode"
+            "currency": "$priceFormat.isoCode",
+            "targeting": "$targeting"
           }
         },
         total: { $sum: 1 }
       }
     },
     {
-      $sort: { total: -1 }
+      $sort: { "_id.origin": 1 }
     }
 
   ])
 }
 
+
 const handler = async (event, context) => {
   try {
     // let tenants = await mongoHelper.getAllSearchEngineDbs()
-    let tenants = ["jl"]
+    let tenants = ["a3"]
     const reportForClient = `client-report_${new Date().toLocaleDateString().replace(/\//g, "_")}.csv`
     const reportForDev = `dev-report_${new Date().toLocaleDateString().replace(/\//g, "_")}.csv`
     for (const tenant of tenants) {
 
       let stringReport = `Routes report without prices ${new Date().toLocaleDateString()}`
-      stringReport += "\ntenant, Total routes without prices"
+      stringReport += "\ntenant, Total origins without prices"
       try {
         const { journeyType, lookaheadWindow, namingConvention } = await req.getSettings(tenant)
         console.log({ journeyType, lookaheadWindow })
 
         const routes = await getDocsAggregation(tenant)
+        const aggregatedByOrigin = {}
+        routes.forEach((b) => {
+          if (aggregatedByOrigin[`${b._id.origin}`] == undefined) {
+            aggregatedByOrigin[`${b._id.origin}`] = { data: b.data, destinations: [b._id.destination] }
+          } else {
+            aggregatedByOrigin[`${b._id.origin}`].data = aggregatedByOrigin[`${b._id.origin}`].data.concat(b.data)
+            aggregatedByOrigin[`${b._id.origin}`].destinations.push(b._id.destination)
+
+          }
+        })
         stringReport += `\n${tenant},${routes.length}\n`
-        console.log(`${tenant},${routes.length}`)
+        // console.log(`${tenant},${routes.length}`)
         await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
-        stringReport = `\nOrigin, Destination, Currency, Available currencies, Report Notes`
+        stringReport = `\nOrigin, Destination, Currency, Available currencies, Report Notes, Recommendation`
         await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, stringReport)
-        let i = 0;
-        console.log(`${routes.length} routes`)
+        stringReport = namingConvention ? `\nOrigin, Destination, Currency, Available currencies, campaignId, campaignName` : `\nOrigin, Destination, Currency, Available currencies, campaignId, campaignName, adgroupId, adgroupName`
+        await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, stringReport)
+        // console.log(`${routes.length} routes`)
         const routesToQueryAdgroups = []
-        for (const route of routes) {
-          console.log(`Origin:${route._id.origin} with ${route.destinations.length} destinations`)
-          const arrayOutput = inChunks(route.destinations, 20);
-          const pricesResultArray = await Promise.all(arrayOutput.map(chunks => req.getPrices(tenant, journeyType, lookaheadWindow, route._id.origin, chunks.map(dest => dest.destination))))
-          const priceResultFlatted = pricesResultArray.flatMap(priceArr => priceArr) // flatted sputnik Results
+        for (const origin of Object.keys(aggregatedByOrigin)) {
+          console.log(`Origin:${origin} with ${aggregatedByOrigin[origin].destinations.length} destinations`)
+          const destinationsInChunks = inChunks(aggregatedByOrigin[origin].destinations, 20);
+          const pricesResultArray = await Promise.all(destinationsInChunks.map(chunks => req.getPrices(tenant, journeyType, lookaheadWindow, origin, chunks.map(dest => dest.destination))))
+          const priceResultFlatted = pricesResultArray.flatMap(priceArr => priceArr)
           let fileBuffer1 = ""
           let fileBuffer2 = ""
-          route.destinations.forEach((destInfo) => {
+          let fileBuffer3 = ""
+          aggregatedByOrigin[origin].destinations.forEach((destination, key) => {
+            // console.log(`Origin:${origin}, Destination: ${destination}`)
+            const OAndD = aggregatedByOrigin[origin].data.filter(route => route.origin == origin && route.destination == destination)
             if (!priceResultFlatted.length) {
-              fileBuffer1 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},0, No prices found with this currency`
+              fileBuffer1 += `\n${origin},${destination},Any currency,, Route not found, Adgroups using this route shouldn't have Ads with prices`
+              fileBuffer3 += OAndD.map((o_d) => `\n${o_d.origin},${o_d.destination},${o_d.currency},, ${Object.values(o_d.targeting).join(",")}`)
             } else {
-              const availableCurrencies = new Array(...new Set(priceResultFlatted.filter(price => price.outboundFlight.arrivalAirportIataCode == destInfo.destination).map(price => price.priceSpecification.currencyCode)))
-              const curcAvailable = availableCurrencies.length && req.isCurrencyAvailable(destInfo.currency, availableCurrencies)
-              const notes = curcAvailable ? "Review by DevTeam"
-                : "Low probability of this route having prices - update to a available currency if existent"
-              if (curcAvailable) {
-                fileBuffer2 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}`
-              } else {
-                fileBuffer1 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}`
-              }
 
-              if (!curcAvailable && availableCurrencies.length) {
-                routesToQueryAdgroups.push({ query: { "routeIdentifier.o": route._id.origin, "routeIdentifier.d": destInfo.destination, "routeIdentifier.curC": destInfo.currency, mapped: true }, availableCurrencies })
-              }
+              const availableCurrencies = new Array(...new Set(priceResultFlatted.filter(price => price.outboundFlight.departureAirportIataCode == origin && price.outboundFlight.arrivalAirportIataCode == destination).map(price => price.priceSpecification.currencyCode)))
+              OAndD.forEach((o_d) => {
+                let notes = availableCurrencies.length ? (req.isCurrencyAvailable(o_d.currency, availableCurrencies) ? " Dev needs to review" : "Route was found but with other currencies, The origin/destination/currency may not be valid") : "Route not found, Adgroups using this route shouldn't have Ads with prices "
+                if (req.isCurrencyAvailable(o_d.currency, availableCurrencies)) {
+                  fileBuffer2 = `\n${o_d.origin},${o_d.destination},${o_d.currency}, ${availableCurrencies.join("/")}, ${notes}`
+                } else {
+                  fileBuffer1 = `\n${o_d.origin},${o_d.destination},${o_d.currency}, ${availableCurrencies.join("/")}, ${notes}`
+                  fileBuffer3 = `\n${o_d.origin},${o_d.destination},${o_d.currency}, ${availableCurrencies.join("/")}, ${Object.values(o_d.targeting).join(",")}`
+                }
+              })
             }
-          }) // flatted empty routes
+
+          })
           await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, fileBuffer1)
           await fHandler.appendToFile(`./reports/${tenant}-${reportForDev}`, fileBuffer2)
+          await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, fileBuffer3)
         }
+
+        // for (const route of routes) {
+        //   console.log(`Origin:${route._id.origin} with ${route.destinations.length} destinations`)
+        //   const arrayOutput = inChunks(route.destinations, 20);
+        //   const pricesResultArray = await Promise.all(arrayOutput.map(chunks => req.getPrices(tenant, journeyType, lookaheadWindow, route._id.origin, chunks.map(dest => dest.destination))))
+        //   const priceResultFlatted = pricesResultArray.flatMap(priceArr => priceArr) // flatted sputnik Results
+        //   let fileBuffer1 = ""
+        //   let fileBuffer2 = ""
+        //   route.destinations.forEach((destInfo) => {
+        //     if (!priceResultFlatted.length) {
+        //       fileBuffer1 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},, No prices available in the next 365 days`
+        //     } else {
+        //       const availableCurrencies = new Array(...new Set(priceResultFlatted.filter(price => price.outboundFlight.arrivalAirportIataCode == destInfo.destination).map(price => price.priceSpecification.currencyCode)))
+        //       const curcAvailable = availableCurrencies.length && req.isCurrencyAvailable(destInfo.currency, availableCurrencies)
+        //       const notes = curcAvailable ? ""
+        //         : (availableCurrencies.length == 0 ? "No prices available in the next 365 days" : "Update to a available currency")
+        //       if (curcAvailable) {
+        //         fileBuffer2 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}`
+        //       } else {
+        //         fileBuffer1 += `\n${route._id.origin},${destInfo.destination},${destInfo.currency},${availableCurrencies.join("/")}, ${notes}`
+        //       }
+
+        //       if (!curcAvailable && availableCurrencies.length) {
+        //         routesToQueryAdgroups.push({ query: { "routeIdentifier.o": route._id.origin, "routeIdentifier.d": destInfo.destination, "routeIdentifier.curC": destInfo.currency, mapped: true }, availableCurrencies })
+        //       }
+        //     }
+        //   }) // flatted empty routes
+        //   await fHandler.appendToFile(`./reports/${tenant}-${reportForClient}`, fileBuffer1)
+        //   await fHandler.appendToFile(`./reports/${tenant}-${reportForDev}`, fileBuffer2)
+        // }
         // for naming convention - report campaigns only
-        if (routesToQueryAdgroups.length) {
-          console.log(`${routesToQueryAdgroups.length} queries`)
-          if (namingConvention) {
-            await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, "Account Id, Campaign Id, Campaign Name, Currency Used, Available Currency")
-          } else {
-            await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, "Account Id, Campaign Id, Campaign Name,Adgroup ID, AdGroup Name, Currency Used, Available Currency")
-          }
-          const routesChunk = inChunks(routesToQueryAdgroups, 5);
-          // const dbQueries = routesChunk.map((adgroups) => {
-          for (const routesArray of routesChunk) {
-            let fileBuffer3 = ""
-            const fullfiledCampaigns = await Promise.all(routesArray.map(data => getAdgAggregation(tenant, namingConvention, data.query)))
-            // console.log({ fullfiledCampaigns })
-            routesArray.forEach((route, index) => {
-              fileBuffer3 += fullfiledCampaigns[index].flatMap(x => x.campaigns).map((dest) => {
-                return namingConvention ?
-                  `\n${dest.accountId},${dest.campaignId},${dest.campaignName},${route.query["routeIdentifier.curC"]}, ${route.availableCurrencies.join("/")}`
-                  : `\n${dest.accountId},${dest.campaignId},${dest.campaignName}, ${dest.adgroupId}, ${dest.adgroupName},${route.query["routeIdentifier.curC"]}, ${route.availableCurrencies.join("/")}`
-              }).join("\n")
-            })
-            await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, fileBuffer3)
-          }
-        }
+        // if (routesToQueryAdgroups.length < 200) {
+        //   console.log(`${routesToQueryAdgroups.length} queries`)
+        //   if (namingConvention) {
+        //     await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, "Account Id, Campaign Id, Campaign Name, Currency Used, Available Currency")
+        //   } else {
+        //     await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, "Account Id, Campaign Id, Campaign Name,Adgroup ID, AdGroup Name, Currency Used, Available Currency")
+        //   }
+        //   const routesChunk = inChunks(routesToQueryAdgroups, 100);
+        //   // const dbQueries = routesChunk.map((adgroups) => {
+        //   for (const routesArray of routesChunk) {
+        //     let fileBuffer3 = ""
+        //     const fullfiledCampaigns = await Promise.all(routesArray.map(data => getAdgAggregation(tenant, namingConvention, data.query)))
+        //     // console.log({ fullfiledCampaigns })
+        //     routesArray.forEach((route, index) => {
+        //       fileBuffer3 += fullfiledCampaigns[index].flatMap(x => x.campaigns).map((dest) => {
+        //         return namingConvention ?
+        //           `\n${dest.accountId},${dest.campaignId},${dest.campaignName},${route.query["routeIdentifier.curC"]}, ${route.availableCurrencies.join("/")}`
+        //           : `\n${dest.accountId},${dest.campaignId},${dest.campaignName}, ${dest.adgroupId}, ${dest.adgroupName},${route.query["routeIdentifier.curC"]}, ${route.availableCurrencies.join("/")}`
+        //       }).join("\n")
+        //     })
+        //     await fHandler.appendToFile(`./reports/${tenant}-detailed-${reportForClient}`, fileBuffer3)
+        //   }
+        // } else {
+        //   console.log("Query too big. run separately")
+        // }
       } catch (error) {
         console.log(error);
         continue;
